46371f1f47c20921d1fe448f95c8237bf55dd048
Alex Awwiller <alexaawwiller@gmail.com>
2023-01-24 12:38:20 -0500

    did some of the steps up until like 7.1

--- project-two/src/reset.js
+++ project-two/src/reset.js
@@ -16,12 +16,27 @@ await db.query(`
   );
 `);
 
-// TODO-Task-1.1: Add VIN column to cars table
 
-// TODO-Task-2.1: Create the owners table
+await db.query(`
+  ALTER TABLE cars
+  ADD COLUMN vin varchar(17) DEFAULT NULL;
+`);
+await db.query(`
+  ALTER TABLE cars
+  ADD COLUMN owner_Id integer DEFAULT NULL;
+`);
 
-// TODO-Task-3.1: Add ownerId foreign key to cars table
+await db.query(`
+  CREATE TABLE owners(
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL
+  );
+`);
 
+await db.query(`
+  ALTER TABLE cars
+  ADD CONSTRAINT fk_cars_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+`);
 const owners = [
   { id: 1, name: "Bob" },
   { id: 2, name: "Jim" },
@@ -51,18 +66,19 @@ const cars = [
   },
 ];
 
-// TODO-Task-2.2: Insert owners into owners table
 
+for (const owner of owners) {
+  await db.query("INSERT INTO owners(id, name) VALUES ($1, $2)", [owner.id, owner.name]);
+};
 // Insert cars into cars table
 for (const car of cars) {
   console.log("Inserting car:", car);
   await db.query(
-    // TODO-Task-1.2 add vin column data
-    // TODO-Task-3.2 add ownerId foreign key data
-    "INSERT INTO cars(year, make, model) VALUES($1, $2, $3)",
-    [car.year, car.make, car.model]
+    "INSERT INTO cars(year, make, model, vin, owner_id) VALUES($1, $2, $3, $4, $5)",
+    [car.year, car.make, car.model, car.vin, car.ownerId]
   );
-}
+};
+
 // Print the progress of our work
 console.log(`Done inserting ${cars.length} cars.`);
 
