045e785001331809ca7565c91b0d9e16b73845a9
Dylan Perkins <djp0826@gmail.com>
2023-01-24 12:37:26 -0500

    Things are working

--- project-two/src/reset.js
+++ project-two/src/reset.js
@@ -17,11 +17,29 @@ await db.query(`
 `);
 
 // TODO-Task-1.1: Add VIN column to cars table
+await db.query(`
+  ALTER TABLE cars
+  ADD COLUMN vin varchar(17) DEFAULT NULL;
+`);
+
+await db.query(`
+  ALTER TABLE cars
+  ADD COLUMN owner_id integer DEFAULT NULL;
+`);
 
 // TODO-Task-2.1: Create the owners table
+await db.query(`
+  CREATE TABLE owners(
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL
+  );
+`);
 
 // TODO-Task-3.1: Add ownerId foreign key to cars table
-
+await db.query(`
+  ALTER TABLE cars
+  ADD CONSTRAINT fk_cars_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+`);
 const owners = [
   { id: 1, name: "Bob" },
   { id: 2, name: "Jim" },
@@ -52,15 +70,21 @@ const cars = [
 ];
 
 // TODO-Task-2.2: Insert owners into owners table
-
+for (const owner of owners) {
+  // What goes here? Look at the other for-loop as an example
+  await db.query(
+    "INSERT INTO owners(id, name) VALUES($1, $2)",
+    [owner.id, owner.name]
+  )
+}
 // Insert cars into cars table
 for (const car of cars) {
   console.log("Inserting car:", car);
   await db.query(
     // TODO-Task-1.2 add vin column data
     // TODO-Task-3.2 add ownerId foreign key data
-    "INSERT INTO cars(year, make, model) VALUES($1, $2, $3)",
-    [car.year, car.make, car.model]
+    "INSERT INTO cars(year, make, model, vin, owner_id) VALUES($1, $2, $3, $4, $5)",
+    [car.year, car.make, car.model, car.vin, car.ownerId]
   );
 }
 // Print the progress of our work
--- project-two/src/server.js
+++ project-two/src/server.js
@@ -11,15 +11,83 @@ app.get("/", async (req, res) => {
   const result = await db.query("select * from cars;");
 
   // TODO-Task-6.1: Replace with example HTML cars page
-  res.json(result.rows);
+  // NOTE: By default, calling `res.send(...)` with a string tells the server "send this string to browser as HTML"
+  // See docs here: https://expressjs.com/en/4x/api.html#res.send
+  res.send(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cars | Project 2</title>
+  </head>
+  <body>
+    <main style="max-width: 800px; margin-left: auto; margin-right: auto;">
+      <h1>Cars</h1>
+      <ol>${result.rows.map(r => (`<li>${r.year} ${r.make} ${r.model}</li>`)).join('')}</ol>
+      <h3>Add a new Car</h3>
+      <form method="POST" action="/car">
+        <label>
+          Year
+          <input type="number" name="year" />
+        </label>
+        <label>
+          Make
+          <input type="text" name="make" />
+        </label>
+        <label>
+          Model
+          <input type="text" name="model" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </main>
+  </body>
+</html>
+`);
 });
 
 // TODO-Task-4.1: Add new GET endpoint /owners
-
+app.get('/owners', async (req, res) => {
+  const result = await db.query("select * from owners;");
+  res.json(result.rows);
+})
 // TODO-Task-5.1: Add new GET endpoint /cars
-
+app.get('/cars', async (req, res) => {
+  const result = await db.query("select * from cars left join owners on cars.owner_id = owners.id;");
+  res.json(result.rows);
+})
 // TODO-Task-7.1: Add new POST endpoint /car
+// NOTE: `app.post(...)` means "I only want to handle HTTP POST requests here"
+// while `app.get(...)` means "I only want to handle HTTP GET requests here"
+app.post('/car', async (req, res) => {
+  // Read the HTTP request body which should contain the form data submitted by
+  // the browser.
+  // NOTE: See docs here: https://expressjs.com/en/4x/api.html#req.body
+  const formData = req.body
 
+  console.log('Form Data:', formData) // this is for our own debugging...
+
+  // TODO-Task-7.2: Write an INSERT statement to save the data into the database
+  await db.query(
+    "INSERT INTO cars(year, make, model) VALUES($1, $2, $3)",
+    [formData.year, formData.make, formData.model]
+  )
+  // Redirect the browser back to the `/cars-html` page, which is what you want to do
+  // when responding to a browser HTML form submission.
+  // NOTE: See docs here: https://expressjs.com/en/4x/api.html#res.redirect
+  res.redirect('/')
+
+  // NOTE: If we were handling a programmatic API submission (like from a
+  // JavaScript or mobile app), we could have instead returned an HTTP 201 status
+  // code indicating "Successfully Created" and some JSON message like so:
+  //
+  //
+  // res.status(201).json({message: "Success!"});
+  //
+  //
+  // Learn more about status codes here: https://www.restapitutorial.com/httpstatuscodes.html
+})
 app.listen(port, () => {
   console.log(`Project-2 app listening on port ${port}`);
 });
